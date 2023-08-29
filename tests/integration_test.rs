8a1a26f0e40684a5ea09f9cd94fe8e717ff181d2
Shane Witbeck <shane@digitalsanctum.com>
2023-08-29 11:32:18 -0700

    feat: allow custom request headers

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,9 @@ extern crate webpage;
 
 #[cfg(feature = "curl")]
 use webpage::{html::HTML, Webpage, WebpageOptions};
+use curl::easy::List;
+use wiremock::{Mock, MockServer, ResponseTemplate};
+use wiremock::matchers::{header, method};
 
 #[test]
 fn from_file() {
@@ -25,4 +28,67 @@ fn from_url() {
     let html = webpage.unwrap().html;
     assert_eq!(html.title, Some("Example Domain".to_string()));
     assert!(html.description.is_none());
+}
+
+#[tokio::test]
+#[cfg(feature = "curl")]
+async fn from_url_with_title() {
+    let mock_server = MockServer::start().await;
+    let response_template = ResponseTemplate::new(200)
+        .set_body_string("<html><head><title>testing</title></head><body></body></html>");
+    Mock::given(method("GET"))
+        .respond_with(response_template)
+        .up_to_n_times(1)
+        .expect(1)
+        .named("from_url_with_title GET")
+        .mount(&mock_server)
+        .await;
+
+    let webpage = Webpage::from_url(&mock_server.uri(), WebpageOptions::default());
+    assert!(webpage.is_ok());
+
+    let html = webpage.unwrap().html;
+    assert_eq!(html.title, Some("testing".to_string()));
+    assert!(html.description.is_none());
+}
+
+#[tokio::test]
+#[cfg(feature = "curl")]
+async fn from_url_with_headers() {
+    let mock_server = MockServer::start().await;
+    Mock::given(method("GET"))
+        .and(header("X-My-Header", "1234"))
+        .respond_with(ResponseTemplate::new(200))
+        .up_to_n_times(1)
+        // We expect the mock to be called exactly once.
+        .expect(1)
+        // We assign a name to the mock - it will be shown in error messages
+        // if our expectation is not verified!
+        .named("from_url_with_headers GET")
+        .mount(&mock_server)
+        .await;
+
+    let custom_header = "X-My-Header: 1234";
+    let mut my_headers = List::new();
+    my_headers.append(custom_header).unwrap();
+    let options = WebpageOptions { headers: my_headers, ..Default::default() };
+    let url = &mock_server.uri();
+    let webpage = Webpage::from_url(url, options);
+    assert!(webpage.is_ok());
+}
+
+#[tokio::test]
+#[cfg(feature = "curl")]
+async fn from_url_without_headers() {
+    let mock_server = MockServer::start().await;
+    Mock::given(method("GET"))
+        .respond_with(ResponseTemplate::new(200))
+        .up_to_n_times(1)
+        .expect(1)
+        .named("from_url_without_headers GET")
+        .mount(&mock_server)
+        .await;
+
+    let webpage = Webpage::from_url(&mock_server.uri(), WebpageOptions::default());
+    assert!(webpage.is_ok());
 }
